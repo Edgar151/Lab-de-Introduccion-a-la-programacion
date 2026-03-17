3607906f8ab77399db2b033634b19846ba18f41e
Edgar151 <134555034+Edgar151@users.noreply.github.com>
2026-03-17 11:35:35 -0600

    Update menu-ejercicios.py

--- ejercicio-8/menu-ejercicios.py
+++ ejercicio-8/menu-ejercicios.py
@@ -51,35 +51,88 @@ def tablas_de_multiplicar():
         print("9 * ",i,"=", i*9)
         print("10 * ",i,"=", i*10)
 
-def triagulo():
-    triangulo= input("Ingresa un numero para crear tu triangulo: ")
-    for i in range(1, 6):
-        print(triangulo * int(random))
+def triangulo():
+    numero = int(input("Ingresa un numero: "))
+    for i in range(5):
+        actual = numero
+        while actual > 0:
+            print(actual, end=" ")
+            actual -= 2
+        print()
+        numero += 2
 
-print("Ingresa 1 para repetir una palabra 10 veces\nIngresa 2 pasa saber cuantos años haz cumplido")
-print("Ingresa 3 para saber numeros impares\nIngresa 4 pasa saber numeros anteriores ")
-print("Ingresa 5 para saber la cantidad de capital de una invercion\nIngresa 6 para triangulo rectangulo")
-print("Ingresa 7 para trablas de multiplicar\nIngresa 8 para triangulo rectangulo de numeros")
-print("Ingresa 9 para validar contraseña\nIngresa 10 para saber numero primo")
-opcion = int(input("Ingresa 11 pasa sbaer si tu numero es numero primo\nIngresa 12 pasa saber la letra de la frase\nIngresa 13 para mirar eco y poder salir:\n"))
+def contraseña():
+    contra = contraseña
+    contraseña1 = input("Ingresa tu contraseña: ")
+    if contra == contraseña1:
+        print("Acceso consedido")
+    else:
+        print("Acceso denegado, intenta otra vez")
+
+def quees():
+    numer = int(input("Ingresa un numero: "))
+    numer = numer % 2
+    if numer == 0:
+       print(" tu numero es par")
+       478
+    else:
+        print(" tu numero es impar") 
+
+def palabrainversa():
+    frase = input("Ingresa una frase: ")
+    invertido = frase[::-1]
+    print(invertido)
     
-match opcion:
-    case 1:
-        palabra10()
-    case 2:
-        edad_cumplida()
-    case 3:
-        numeros_impares()
-    case 4:
-        cuenta_atras()
-    case 5:
-        interes()
-    case 6:
-        triagulo_rectangulo()
-    case 7: 
-        tablas_de_multiplicar()
-    case 8:
+def numerodeletras():
+    frase = input("Ingresa una frase: ")
+    letra = input("Ingresa una letra: ")
+    veces = frase.count(letra)
+    print(veces)
 
-    case _:
-        print("Opcion no valida")
+def eco():
+    while True:
+        palabra = input("Ingresa la palabra que quieres en eco\n o escribe salir para salir: ")
+        if palabra == "salir":
+            break
+        else:
+            print(palabra)
 
+while True:
+    print("Ingresa 1 para repetir una palabra 10 veces\nIngresa 2 pasa saber cuantos años haz cumplido")
+    print("Ingresa 3 para saber numeros impares\nIngresa 4 pasa saber numeros anteriores ")
+    print("Ingresa 5 para saber la cantidad de capital de una invercion\nIngresa 6 para triangulo rectangulo")
+    print("Ingresa 7 para trablas de multiplicar\nIngresa 8 para triangulo rectangulo de numeros")
+    print("Ingresa 9 para validar contraseña\nIngresa 10 para saber numero primo")
+    opcion = int(input("Ingresa 11 Para invertir una frase\nIngresa 12 pasa saber la letra de la frase\nIngresa 13 para mirar eco\nIngresa 14 para salir: "))
+
+    match opcion:
+        case 1:
+            palabra10()
+        case 2:
+            edad_cumplida()
+        case 3:
+            numeros_impares()
+        case 4:
+            cuenta_atras()
+        case 5:
+            interes()
+        case 6:
+            triagulo_rectangulo()
+        case 7: 
+            tablas_de_multiplicar()
+        case 8:
+            triangulo()
+        case 9:
+            contraseña()
+        case 10:
+            quees()
+        case 11:
+            palabrainversa()
+        case 12:
+            numerodeletras()
+        case 13:
+            eco()
+        case 14:
+            break
+        case _:
+            print("Opcion no valida")
